fb1c2683823d58dc8eb5bdb654ee850fe0bcfa2a
Joseph Perkins <gamerdew64@gmail.com>
2018-09-10 21:40:31 -0500

    initial index page structure and start of schema and seeds sql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO movies (movie_name, is_movie_selected) VALUES ('name' true);
+INSERT INTO movies (movie_name, is_movie_selected) VALUES ('name' false);
+INSERT INTO animals (animal_name, is_animal_selected) VALUES ('name', true);
+INSERT INTO animals (animal_name, sleepy) VALUES ('name', false);
+INSERT INTO athletes (athlete_name, sleepy) VALUES ('name', true);
+INSERT INTO athletes (athlete_name) VALUES ('name' false);
