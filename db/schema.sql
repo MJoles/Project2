89f56c5f9b1c9e0f9a9019c3cd1818fa9d1f195d
Joseph Perkins <gamerdew64@gmail.com>
2018-09-12 18:33:52 -0500

    small changes to index page and rough template of the db structure

--- db/schema.sql
+++ db/schema.sql
@@ -3,26 +3,39 @@
 CREATE DATABASE votingDB;
 USE votingDB;
 
-CREATE TABLE category
+CREATE TABLE brackets
 (
-	category_id int NOT NULL AUTO_INCREMENT,
-	category_name varchar(100) NOT NULL,
-	choice_id int(100),
-	choice_name int(100),
-	is_selected BOOLEAN DEFAULT false,
-	PRIMARY KEY (category_id)
+	id INT NOT NULL AUTO_INCREMENT,
+	category_name VARCHAR(100) NOT NULL,
+	-- is_brackets_selected BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
 );
 
-CREATE TABLE bracketItems
+CREATE TABLE choices
 (
-	bracketItem_id int NOT NULL AUTO_INCREMENT,
-	url varchar(255) NOT NULL,
-	PRIMARY KEY (bracketItem_id)
+	id INT NOT NULL AUTO_INCREMENT,
+	bracket_id INT(100),
+	choice_name INT(100),
+	choice_url VARCHAR(255) NOT NULL,
+	-- is_choices_selected BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE votes
 (
-	votes_id int NOT NULL AUTO_INCREMENT,
-	vote int(255),
-	PRIMARY KEY (votes_id)
+	id INT NOT NULL AUTO_INCREMENT,
+	user_id INT,
+	bracket_id INT,
+	vote_name INT(255),
+	round_number INT,
+	-- vote_number INT,
+	-- is_votes_selected BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE users
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	user_name INT,
+	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,44 @@
-INSERT INTO movies (movie_name, is_movie_selected) VALUES ('name' true);
-INSERT INTO movies (movie_name, is_movie_selected) VALUES ('name' false);
-INSERT INTO animals (animal_name, is_animal_selected) VALUES ('name', true);
-INSERT INTO animals (animal_name, sleepy) VALUES ('name', false);
-INSERT INTO athletes (athlete_name, sleepy) VALUES ('name', true);
-INSERT INTO athletes (athlete_name) VALUES ('name' false);
+INSERT INTO votingDB.mainCategory (mainCategory_name, is_mainCategory_selected) VALUES ('Movies', true);
+INSERT INTO votingDB.mainCategory (mainCategory_name, is_mainCategory_selected) VALUES ('Animals', true);
+INSERT INTO votingDB.mainCategory (mainCategory_name, is_mainCategory_selected) VALUES ('Athletes', true);
+
+INSERT INTO votingDB.subCategory (category_name, is_category_selected) VALUES ('Superhero Movies', true);
+
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('The Dark Knight', '', true);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('Captain America: The Winter Soldier', '', false);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('Spiderman 2', '', true);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('Superman (1978)', '', false);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('The Avengers', '', true);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('Batman Begins', '', false);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('Guardians of the Galaxy', '', true);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('Logan', '', false);
+
+INSERT INTO votingDB.subCategory (category_name, is_category_selected) VALUES ('Horror Movies', true);
+
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('Halloween (1978)', '', false);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('It Follows', '', true);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('The Shining', '', false);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('Psycho (1960)', '', true);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('Scream', '', false);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('The Cabin in the Woods', '', true);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('A Nightmare on Elm Street (1984)', '', false);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('Scream 3', '', true);
+
+INSERT INTO votingDB.subCategory (category_name, is_category_selected) VALUES ('Comedy Movies', false);
+
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('Monty Python and the Holy Grail', '', false);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('Airplane', '', false);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('Groundhog Day', '', false);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('Ghostbusters', '', false);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('The Hangover', '', false);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('Dumb and Dumber', '', false);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('Vacation', '', false);
+INSERT INTO votingDB.choice (choice_name, choice_url, is_choice_selected) VALUES ('Office Space', '', false);
+
+INSERT INTO votingDB.vote (vote_name, vote_total, is_vote_selected) VALUES ('Airplane', 5, true);
+INSERT INTO votingDB.vote (vote_name, vote_total, is_vote_selected) VALUES ('Airplane', 5, true);
+INSERT INTO votingDB.vote (vote_name, vote_total, is_vote_selected) VALUES ('Airplane', 5, true);
+INSERT INTO votingDB.vote (vote_name, vote_total, is_vote_selected) VALUES ('Airplane', 5, true);
+INSERT INTO votingDB.vote (vote_name, vote_total, is_vote_selected) VALUES ('Airplane', 5, true);
+INSERT INTO votingDB.vote (vote_name, vote_total, is_vote_selected) VALUES ('Airplane', 5, true);
+INSERT INTO votingDB.vote (vote_name, vote_total, is_vote_selected) VALUES ('Airplane', 5, true);
