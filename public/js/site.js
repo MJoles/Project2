6a6c093826a69e8397b3f8ed642721cefc6a2a15
Mandy <mandyjjoles@gmail.com>
2018-09-21 00:23:40 -0500

    submit button onclick handler working to post user choices to RoundOne database

--- public/js/site.js
+++ public/js/site.js
@@ -1,3 +1,4 @@
+$(document).ready(function() {
 var roundOneUserPicks = {
     choiceOne: "",
     choiceTwo: "",
@@ -15,28 +16,29 @@ $('.votingbutton').on('click', function () {
     console.log(roundOneUserPicks.choiceTwo);
     console.log(roundOneUserPicks.choiceThree);
     console.log(roundOneUserPicks.choiceFour);
+    console.log("voting button" + roundOneUserPicks);
     var id = $(this).data("id");
-
+    console.log(id);
     //figure out which match you're in
     //then get id that they clicked
     //update the userPicks vars 
-    if (match === "1") {
+    if (match == "1") {
         roundOneUserPicks.choiceOne = id;
     }
-    else if (match === "2") {
+    else if (match == "2") {
         roundOneUserPicks.choiceTwo = id;
     }
-    else if (match === "3") {
+    else if (match == "3") {
         roundOneUserPicks.choiceThree = id;
     }
-    else if (match === "4") {
+    else if (match == "4") {
         roundOneUserPicks.choiceFour = id;
     }
 
 });
 
 //on click handle for the submit button
-$(".submitButton").on('click', function (userPicks) {
+$(".submitButton").on('click', function () {
 
     // ensure strings arent empty
     // if (roundOneUserPicks.choiceOne === "" || roundOneUserPicks.choiceTwo === "" || roundOneUserPicks.choiceThree === "" || roundOneUserPicks.choiceFour === "") {
@@ -45,19 +47,28 @@ $(".submitButton").on('click', function (userPicks) {
     // }
     // else {
         //make ajax post request
-        $.post("/rd2", userPicks)
-        console.log(userPicks)
-        //.then redirect them to next page 
-        .then(RoundOne);   
+//         $.post("/rd2", {myData: roundOneUserPicks.ch}, function (data, status, xhr){
+//             alert('status: ' + status + ", data: " + data)
+//         })
+       console.log(roundOneUserPicks)
+//         //.then redirect them to next page 
+//         .then(RoundOne); return RoundOne
+        
 
 
+// });
+
+$.ajax({
+    headers: {
+          "Content-Type": "application/json"
+        },
+       type: "POST",
+       url: "/rd2",
+       data: JSON.stringify(roundOneUserPicks)
+         
+ }).then(function(res) {
+   console.log(res)
 });
-
-//   $.ajax({
-    // headers: {
-    //       "Content-Type": "application/json"
-    //     },
-    //     type: "POST",
-    //     url: "/rd2",
-    //     data: JSON.stringify(example)
-//   });
+  console.log("this");
+});
+});
