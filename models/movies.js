f60ceaf4c73566ae25d7fd6f76ffcfe4e5661fd0
Joseph Perkins <gamerdew64@gmail.com>
2018-09-22 20:19:26 -0500

    more comments

--- models/movies.js
+++ models/movies.js
@@ -3,6 +3,7 @@ module.exports = function (sequelize, DataTypes) {
   var Movie = sequelize.define("Movie", {
     movieTitle: DataTypes.STRING,
     posterURL: DataTypes.STRING
+    // ultimately decided against using this boolean field when a user selects a movie
     //isSelected: DataTypes.BOOLEAN
   });
   return Movie;
